a8e86762bce7599e3d46f980455c9c16d5e33aac
John Gallaugher <john.gallaugher@bc.edu>
2020-02-09 13:24:52 -0500

    EU through Ch. 5.6 Structs

--- EU/DetailTableViewController.swift
+++ EU/DetailTableViewController.swift
@@ -12,9 +12,24 @@ class DetailTableViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
     @IBOutlet weak var countryField: UITextField!
     @IBOutlet weak var capitalField: UITextField!
+    @IBOutlet weak var euroSwitch: UISwitch!
+    
+    var nation: Nation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if nation == nil {
+            nation = Nation(country: "", capital: "", usesEuro: false)
+        }
+        
+        countryField.text = nation.country
+        capitalField.text = nation.capital
+        euroSwitch.isOn = nation.usesEuro
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        nation = Nation(country: countryField.text!, capital: capitalField.text!, usesEuro: euroSwitch.isOn)
     }
 
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
---
+++ EU/Nation.swift
@@ -0,0 +1,15 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by John Gallaugher on 2/5/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
